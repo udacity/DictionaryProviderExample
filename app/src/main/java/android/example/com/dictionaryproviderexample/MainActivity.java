887329056248c279ba5a8760b283b9b821b3b7db
Paul <paulgureghian.att.net>
2016-08-13 16:00:05 -0700

    8-13-16  4:00 PM

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package android.example.com.dictionaryproviderexample;
 
 import android.content.ContentResolver;
@@ -23,10 +8,6 @@ import android.provider.UserDictionary.Words;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
-/**
- * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
- * to show an example of accessing the {@link Words} list via its' Content Provider.
- */
 public class MainActivity extends ActionBarActivity {
 
     @Override
@@ -34,13 +15,30 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get the TextView which will be populated with the Dictionary ContentProvider data.
         TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
 
-        // Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
 
-        // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
+
+        try {
+            dictTextView.setText("The user dictionary contains " + cursor.getCount() + " words\n");
+            dictTextView.append("COLUMNS: " + Words._ID + " _ " + Words.FREQUENCY + " _ " + Words.WORD);
+
+            int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
+            int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
+            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
+
+            while (cursor.moveToNext()) {
+
+                int id = cursor.getInt(idColumn);
+                int frequency = cursor.getInt(frequencyColumn);
+                String word = cursor.getString(wordColumn);
+
+                dictTextView.append(("\n" + id + " _ " + frequency + " _ " + word));
+            }
+        } finally {
+            cursor.close();
+        }
     }
 }
